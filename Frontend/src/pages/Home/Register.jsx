742c9533546e95ef1b6713d3eec23706ae13b341
Yuvraj Verma <106587558+yashv-77@users.noreply.github.com>
2024-03-10 01:23:58 +0530

    fixed form validation

--- Frontend/src/pages/Home/Register.jsx
+++ Frontend/src/pages/Home/Register.jsx
@@ -1,14 +1,14 @@
 import {
 	Card,
-	Input,
 	Checkbox,
+	Alert,
 	Button,
 	Typography,
 } from "@material-tailwind/react";
 import BrandName from "../../assets/brandName.svg?react";
 import BrandLogo from "../../assets/brandLogo.svg?react";
-import Footer from "./Footer";
-import { Link } from "react-router-dom";
+
+import { Link, Navigate } from "react-router-dom";
 import RegisterSvg from "../../assets/registerSvg2.svg?react";
 import { useState } from "react";
 import axios from "axios";
@@ -24,21 +24,25 @@ export default function Register() {
 	const [passwordError, setPasswordError] = useState("");
 
 	// validation
-	const [isFormValid, setIsFormValid] = useState(false);
-
+	const [isloading, setIsloading] = useState(false);
+	const [showAlert, setShowAlert] = useState(false);
 	const [isChecked, setIsChecked] = useState("false");
+
 
 	const handleSubmit = async (event) => {
 		event.preventDefault();
-
+		setIsloading(true);
 		// Check if all fields are filled
 		const areFieldsFilled = name && email && password;
+		let formIsValid = true;
 
 		// Validate the name
 		if (!name.trim()) {
 			setNameError("Name is required");
+			formIsValid = false;
 		} else if (name.trim().length < 3) {
 			setNameError("Name must be at least 3 characters");
+			formIsValid = false;
 		} else {
 			setNameError("");
 		}
@@ -46,43 +50,51 @@ export default function Register() {
 		// Validate the email
 		if (!email) {
 			setEmailError("Email is required");
+			formIsValid = false;
 		} else if (!email.includes("@")) {
 			setEmailError("Email is not valid");
+			formIsValid = false;
 		} else {
 			setEmailError("");
 		}
 		// Validate the password
 		if (!password) {
 			setPasswordError("Password is required");
+			formIsValid = false;
 		} else if (password.length < 8) {
 			setPasswordError("Password must be at least 8 characters");
+			formIsValid = false;	
 		} else {
 			setPasswordError("");
 		}
 		// Update the form validity
-		if (areFieldsFilled && !nameError && !emailError && !passwordError) {
+		if (formIsValid && areFieldsFilled) {
 			try {
+				await new Promise(resolve => setTimeout(resolve, 1000));
 				const response = await axios.post('http://localhost:6001/user/register', { name, email, password });
 				console.log(response.data);
 				// Handle the response here
+				setIsloading(false);
+				setShowAlert(true);
+				Navigate('/login');
 			} catch (error) {
-				console.error("Error in registering user", error);
+				console.log("Error in registering user", error);
 				// Handle the error here
 			}
-					
+
 		} else {
-			setIsFormValid(false);
+			setIsloading(false);
 			console.log("Form validation failed");
 		}
 
-	
+
 	};
 
 
 
 	return (
 		<>
-			<div className=" sm:bg-[#eefffa] h-screen">
+			<div className=" sm:bg-[#eefffa] h-screen relative overflow-hidden">
 				<Link to="/" className="hidden sm:block">
 					<div className="flex items-center justify-center p-4">
 						<BrandLogo />
@@ -96,7 +108,7 @@ export default function Register() {
 							<BrandName />
 						</div>
 					</Link>
-					<div className="  grid sm:grid-cols-2 sm:px-36 px-2 py-5 sm:py-16  ">
+					<div className="grid sm:grid-cols-2 sm:px-36 px-2 py-5 sm:py-16  ">
 						{/* Card Container */}
 						<div className="">
 							<Card color="transparent" shadow={false}>
@@ -128,24 +140,13 @@ export default function Register() {
 												Your Name
 											</Typography>
 											<input
-											placeholder="Enter Your Good Name"
-											value={name}
-											type="text"
-											onChange={(e) => setName(e.target.value)}
-											className=" w-full bg-gray-100 text-sm outline-none border-gray-500 border rounded-[21px] p-4 hover:ring-2 focus:ring-2 focus:ring-prime-500"
-										/>
-											{/* <Input
-												size="lg"
-												color="deep-purple"
+												placeholder="Enter Your Good Name"
 												value={name}
+												type="text"
 												onChange={(e) => setName(e.target.value)}
-												placeholder="name@mail.com"
-												className="  !border-blue-gray-200  focus:!border-deep-purple-700 focus:ring hover:ring-2 "
-												labelProps={{
-													className:
-														"font-poppins",
-												}}
-											/> */}
+												className=" w-full bg-gray-100 text-sm outline-none border-gray-500 border rounded-[21px] p-4 hover:ring-2 focus:ring-2 focus:ring-prime-500"
+											/>
+
 											<p className="text-center text-sm h-[20px] text-red-500">{nameError}</p>
 										</div>
 
@@ -158,21 +159,13 @@ export default function Register() {
 												Your Email
 											</Typography>
 											<input
-											placeholder="Your Email"
-											value={email}
-											type="text"
-											onChange={(e) => setEmail(e.target.value)}
-											className=" w-full bg-gray-100 text-sm outline-none border-gray-500 border rounded-[21px] p-4 hover:ring-2 focus:ring-2 focus:ring-prime-500"
-										/>
-											{/* <Input
-												size="lg"
+												placeholder="Your Email"
 												value={email}
+												type="text"
 												onChange={(e) => setEmail(e.target.value)}
-												color="deep-purple"
-												placeholder="name@mail.com"
-												className=" !border-blue-gray-200  focus:!border-deep-purple-700 focus:ring hover:ring-2 "
+												className=" w-full bg-gray-100 text-sm outline-none border-gray-500 border rounded-[21px] p-4 hover:ring-2 focus:ring-2 focus:ring-prime-500"
+											/>
 
-											/> */}
 											<p className="text-sm text-center h-[20px] text-red-500">{emailError}</p>
 										</div>
 
@@ -185,22 +178,13 @@ export default function Register() {
 												Password
 											</Typography>
 											<input
-											placeholder="*******"
-											value={password}
-											type="password"
-											onChange={(e) => setPassword(e.target.value)}
-											className=" w-full bg-gray-100 text-sm outline-none border-gray-500 border rounded-[21px] p-4 hover:ring-2 focus:ring-2 focus:ring-prime-500"
-										/>
-											{/* <Input
-												type="password"
-												size="lg"
-
+												placeholder="*******"
 												value={password}
+												type="password"
 												onChange={(e) => setPassword(e.target.value)}
-												color="deep-purple"
-												placeholder="********"
-												className=" !border-blue-gray-200 focus:!border-deep-purple-700 focus:ring hover:ring-2"
-											/> */}
+												className=" w-full bg-gray-100 text-sm outline-none border-gray-500 border rounded-[21px] p-4 hover:ring-2 focus:ring-2 focus:ring-prime-500"
+											/>
+
 											<p className="text-center h-[20px] text-sm text-red-500">{passwordError}</p>
 										</div>
 									</div>
@@ -225,9 +209,10 @@ export default function Register() {
 
 									<Button
 										type="submit"
-										color="deep-purple"
-										className="mt-1 rounded-full font-poppins py-4"
+
+										className="mt-1 flex justify-center rounded-full bg-[#8c52ff] font-poppins py-4"
 										fullWidth
+										loading={isloading}
 										disabled={isChecked}
 									>
 										Sign up
@@ -252,6 +237,19 @@ export default function Register() {
 						</div>
 					</div>
 				</div>
+				{showAlert && (
+				<div className="absolute bottom-5 right-5">
+					<Alert animate={{
+						mount: { x: 0 },
+						unmount: { x: 200 },
+					}}
+						className="rounded-l-3xl rounded-b-2xl "
+						icon={<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" width="24" height="24" color="#ffffff" fill="none">
+							<path d="M15 2.4578C14.053 2.16035 13.0452 2 12 2C6.47715 2 2 6.47715 2 12C2 17.5228 6.47715 22 12 22C17.5228 22 22 17.5228 22 12C22 10.9548 21.8396 9.94704 21.5422 9" stroke="currentColor" stroke-width="1.5" stroke-linecap="round" />
+							<path d="M8.5 9.5L12 13L21.0002 3" stroke="currentColor" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round" />
+						</svg>}
+						color="green">You Are Succesfully Registered</Alert>
+				</div>)}
 			</div>
 		</>
 	);
