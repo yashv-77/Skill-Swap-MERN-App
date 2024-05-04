10803c028894224dfa903d4f859816e5c7b873e2
Yuvraj Verma <106587558+yashv-77@users.noreply.github.com>
2024-05-04 16:07:40 +0530

    Update DashboardLayout.jsx

--- Frontend/src/pages/App/Dashboard/DashboardLayout.jsx
+++ Frontend/src/pages/App/Dashboard/DashboardLayout.jsx
@@ -1,6 +1,18 @@
 import React from 'react'
 
 function DashboardLayout() {
+<<<<<<< Updated upstream
+=======
+  const { authUser, setAuthUser, selectedChat, setSelectedChat, searchSkill, setSearchSkill } = useContext(AuthContext);
+  const [isloading, setIsloading] = useState(false);
+  const [searchResult, setSearchResult] = useState([]);
+  
+
+  const logOut = () => {
+    localStorage.removeItem('userLocalData');
+    navigate('/')
+}
+>>>>>>> Stashed changes
   return (
     <div>DashboardLayout</div>
   )
